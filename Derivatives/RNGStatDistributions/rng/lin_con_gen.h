3a128091d4d5f5b962e71a597c67fb28056c4e87
Karthik <karthik.jagadeesan@rutgers.edu>
2024-08-19 16:55:09 -0700

    Replace Makefile with CMakeLists.txt for rng

---
+++ Derivatives/RNGStatDistributions/rng/lin_con_gen.h
@@ -0,0 +1,19 @@
+#ifndef __LINEAR_CONGRUENTIAL_GENERATOR_H
+#define __LINEAR_CONGRUENTIAL_GENERATOR_H
+
+#include "random.h"
+
+class LinearCongruentialGenerator : public RandomNumberGenerator {
+ private:
+  double max_multiplier;
+  
+ public:
+  LinearCongruentialGenerator(unsigned long _num_draws, 
+                              unsigned long _init_seed = 1);
+  virtual ~LinearCongruentialGenerator() {};
+  
+  virtual unsigned long get_random_integer();
+  virtual void get_uniform_draws(std::vector<double>& draws); 
+};
+
+#endif
